c1cfb53d83c30a0eb528c691520c000e9c6d94a1
DIR-IT <DIR-IT@DIR-IT-HP>
2018-09-06 11:21:37 -0500

    Proyecto spring curso

---
+++ spring-hola-mundo/src/main/java/com/springholamundo/SpringHolaMundoApplication.java
@@ -0,0 +1,14 @@
+package com.springholamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHolaMundoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringHolaMundoApplication.class, args);
+		
+	}
+}
